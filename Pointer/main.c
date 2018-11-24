35cfc748a06dab9f2e900e834a39f3ee5191018c
harun <harunn.7@hotmail.com>
2018-11-24 15:24:10 +0300

    Initial Commit

---
+++ Pointer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pointer
+//
+//  Created by Harun Öz on 11/24/18.
+//  Copyright © 2018 Harun Öz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
