dbb184f9a35f88614ac464978a1aef4a842a66f1
harun <harunn.7@hotmail.com>
2018-11-26 15:48:41 +0300

    improved

--- Pointer/main.c
+++ Pointer/main.c
@@ -7,9 +7,65 @@
 //
 
 #include <stdio.h>
+#define MAXLEN 50
+#define    NULLCHAR    '\0'
+
+int copyEven(int *a1,int n1, int *a2);
+void disparr(int ar[], int n);
+void copyAlpha(char mySt1[],char mySt2[]);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+//    int A[]={1,2,3,4,5};
+  //  int size= sizeof(A) /sizeof(A[0]);
+   // int total=sumOfElements(A,size);
+    //printf("%d\n", total);
+    
+    
+    int ar1[MAXLEN] = {7, 4, 9, 1, 12, 5, 4, 9, 6, 3, 2, 8, 7, 2};
+  int ar2[MAXLEN];
+    
+    int n=14;
+    int n2;
+    n2= copyEven(ar1, n, ar2);
+    disparr(ar2, n2);
+   //char str1[MAXLEN] = "Abc, defgh -*+- (x + Y + z)\tqwERty098";
+    //char str2[MAXLEN];
+    
+  //  printf("%s\n", str1);
+    //copyAlpha(str1, str2);
+    //printf("%s\n", str2);
+    
+
+
+}
+int sumOfElements(int *A, int size){
+    int j=0, sum=0;
+    for (j=0;j<size;j++){
+        sum += *(A+j);
+    }
+    return sum;
+}
+
+
+void disparr(int ar[], int n){
+    int j;
+    for(j=0; j<n;j++){
+        printf("%d ", ar[j]);
+        
+    }
+    printf("\n");
+}
+
+void copyAlpha(char mySt1[],char mySt2[]){
+    while(*mySt1!= NULLCHAR){
+        if(isalpha(*mySt1)){
+            *mySt2 = *mySt1;
+            mySt2++;
+        }
+        mySt1++;
+    }
+    *mySt2= NULLCHAR;
+        
+    
 }
---
+++ harun.h
@@ -0,0 +1,14 @@
+//
+//  harun.h
+//  Pointer
+//
+//  Created by Harun Öz on 11/24/18.
+//  Copyright © 2018 Harun Öz. All rights reserved.
+//
+
+#ifndef harun_h
+#define harun_h
+
+#include <stdio.h>
+
+#endif /* harun_h */
